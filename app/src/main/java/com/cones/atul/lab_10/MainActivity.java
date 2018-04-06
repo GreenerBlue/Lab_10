a250027e2864a5272e2bdb1c6187a236b5d4c697
Atul <greenerbluer@gmail.com>
2018-04-06 11:01:44 +0530

    Added AlertDialog for message

--- app/src/main/java/com/cones/atul/lab_10/MainActivity.java
+++ app/src/main/java/com/cones/atul/lab_10/MainActivity.java
@@ -1,13 +1,16 @@
 package com.cones.atul.lab_10;
 
 import android.Manifest;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.net.Uri;
-import android.support.v4.app.ActivityCompat;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.ActivityCompat;
+import android.support.v7.app.AlertDialog;
+import android.support.v7.app.AppCompatActivity;
 import android.telephony.SmsManager;
+import android.text.InputType;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.Toast;
@@ -42,8 +45,23 @@ public class MainActivity extends AppCompatActivity {
             Toast.makeText(this,"Failed! Grant permission to continue",Toast.LENGTH_SHORT).show();
             return;
         }
-        SmsManager mgr = SmsManager.getDefault();
-        mgr.sendTextMessage(phone.getText().toString(),null,"This is an automated message.",null,null);
-        Toast.makeText(this,"Sent successfully!",Toast.LENGTH_SHORT).show();
+        if (phone.getText().toString().equals("")){
+            Toast.makeText(this,"Cant message empty number",Toast.LENGTH_SHORT).show();
+            return;
+        }
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Enter message");
+        final EditText input = new EditText(MainActivity.this);
+        input.setInputType(InputType.TYPE_TEXT_FLAG_AUTO_CORRECT);
+        builder.setView(input);
+        builder.setPositiveButton("Send", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                SmsManager mgr = SmsManager.getDefault();
+                mgr.sendTextMessage(phone.getText().toString(), null, input.getText().toString(), null, null);
+                Toast.makeText(getApplicationContext(), "Sent successfully!", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.create().show();
     }
 }
